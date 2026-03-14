dcc6530e7e91ca889f2f07c8b29574953b281d8d
olszewskv <olszewskv@proton.me>
2026-03-14 12:24:09 +0100

    trees with a system of falling leaf particles

--- new-game-project/world.gd
+++ new-game-project/world.gd
@@ -1,10 +1,11 @@
 extends Node3D
 
 @export var bird_count: int = 6
+@export var tree_count: int = 5
 
 var bird_scene = preload("res://bird.tscn")
+var tree_scene = preload("res://tree.tscn")
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	for i in bird_count:
 		var bird = bird_scene.instantiate() 
@@ -14,3 +15,15 @@ func _ready() -> void:
 		bird.speed = randf_range(0.3, 0.7)
 		
 		add_child(bird)
+		
+	for i in tree_count:
+		var tree = tree_scene.instantiate()
+		var angle = randf() * TAU
+		var dist = randf_range(8.0, 20.0)
+		tree.position.x = cos(angle) * dist
+		tree.position.z = sin(angle) * dist
+		tree.position.y = 0.0
+		
+		var scale = randf_range(0.7, 1.3)
+		tree.scale = Vector3(scale, scale, scale)
+		add_child(tree)
